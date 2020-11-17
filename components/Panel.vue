264d05963c9ced01212d616210094281e8b4d012
nathanael <nathanael.bjorkgard@billes.se>
2020-11-17 15:52:05 +0100

    Show ascii code

--- components/Panel.vue
+++ components/Panel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white overflow-hidden shadow rounded-lg w-1/5">
+  <div class="bg-white overflow-hidden shadow rounded-lg w-48 mb-4">
     <div
       class="border-b border-gray-200 bg-purple-500 text-white font-bold px-2 py-2 text-center"
     >
